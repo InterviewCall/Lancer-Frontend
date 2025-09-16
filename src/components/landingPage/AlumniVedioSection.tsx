7c0089238c2eff34c2fded6c93b8eb5168d15135
Pratham <sinhapratham1234@gmail.com>
2025-09-16 13:51:51 +0530

    #fix: fixed video thumbnail

--- src/components/landingPage/AlumniVedioSection.tsx
+++ src/components/landingPage/AlumniVedioSection.tsx
@@ -1,36 +1,38 @@
+import Image from 'next/image';
+
 export default function AlumniVideoSection() {
   const videoContent = [
     {
       id: 1,
-      src: '<video_url_1>',
+      src: '/alumniThumbnail.png',
       heading: 'Meet out student',
       subHeading:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.',
     },
     {
       id: 2,
-      src: '<video_url_2>',
+      src: '/alumniThumbnail.png',
       heading: 'Meet our alumni',
       subHeading:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.',
     },
     {
       id: 3,
-      src: '<video_url_3>',
+      src: '/alumniThumbnail.png',
       heading: 'Meet our faculty',
       subHeading:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.',
     },
     {
       id: 4,
-      src: '<video_url_4>',
+      src: '/alumniThumbnail.png',
       heading: 'Meet our staff',
       subHeading:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.',
     },
     {
       id: 5,
-      src: '<video_url_5>',
+      src: '/alumniThumbnail.png',
       heading: 'Meet our community',
       subHeading:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quas.',
@@ -42,8 +44,8 @@ export default function AlumniVideoSection() {
       {videoContent.map((content) => (
         <div key={content.id} className="my-5 w-1/4  rounded-2xl bg-[#2DF8C5]">
           <div className="p-5">
-            <div className="h-36 border rounded-2xl bg-gray-50">
-              Video Thumbnail
+            <div className="h-fit hover:cursor-pointer rounded-5xl ">
+              <Image alt="" src={content.src} width={400} height={400} />
             </div>
             <div className="font-bold text-lg my-2">{content.heading}</div>
             <div className="text-sm ">{content.subHeading}</div>
